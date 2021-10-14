44ebe0eb3a231c110b3ae034985e39c2d2ab5d4b
Nam Nguyen <87434122+great-great-iy@users.noreply.github.com>
2021-10-14 16:46:33 +0700

    update

--- arts-store/src/App.js
+++ arts-store/src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Header from './components/Header';
 
 function App() {
   return (
     <>
     <Router>
+      <Header/>
       <Switch>
         <Route/>
       </Switch>
